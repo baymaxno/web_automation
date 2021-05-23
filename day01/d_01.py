b5cd6c48715aaf168bc75e31880489ffb3d724c3
dabai <498366331@qq.com>
2021-05-23 19:21:53 +0800

    上传第一次代码

--- day01/d_01.py
+++ day01/d_01.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+from time import sleep
 
 
 # 启动谷歌浏览器哦   版本chromdriver要与谷歌版本一样，且要放到python与谷歌对于的目录下
 driver = webdriver.Chrome()
-dri = driver.get("http://localhost:63342/python13-api-test/day01/dabai_html.html?_ijt=71in36058h4idmdqa57kvf5enr")
+driver.get("http://www.baidu.com")
+
+ele = driver.find_element_by_id("kw")
+
+print(ele)
+
+ele.send_keys("腾讯视频")
+# get_attribute  获取指定属性值
+print(ele.get_attribute("name"))
+
+sleep(3)
+
+
+driver.quit()
